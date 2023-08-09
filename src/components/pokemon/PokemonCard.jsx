6dc2dbc35da97d300de4463e0b3d8ad91b690582
JonaCaucota <jonacau12@gmail.com>
2023-08-09 08:55:08 -0300

    added colors to types

--- src/components/pokemon/PokemonCard.jsx
+++ src/components/pokemon/PokemonCard.jsx
@@ -2,8 +2,8 @@ import {Link} from "react-router-dom";
 import pikaGif from "../../assets/pikachu.gif";
 import {addClassByType} from "../../helpers/addClassByType";
 
-export const PokemonCard = ({id, image, name,types}) => {
-    if(id === null) {
+export const PokemonCard = ({id, image, name, types}) => {
+    if (id === null) {
         return <div className={'row justify-content-center'}>
             <img className={'col-4 pt-5 pb-5'} src={pikaGif} alt={'PokeGif'} style={{width: '16rem', height: '20rem'}}/>
         </div>
@@ -16,16 +16,19 @@ export const PokemonCard = ({id, image, name,types}) => {
     return (
         <>
             <li key={id} className={`col-4 p-4 d-flex align-items-center justify-content-center`}>
-                <div className={`card justify-content-center pt-5 gradient-${className}`} style={{width: '20rem', height: '21rem'}}>
+                <div className={`card justify-content-center pt-2 gradient-${className}`}
+                     style={{width: '20rem', height: '22rem'}}>
                     <Link to={`/pokemon/${id}`} className={'pokemon-card'}>
-                    <div className="backgroundEffect"></div>
-                    <img src={image} className="card-img-top" alt={name}
-                                                                                        style={{width: '12rem', height: '12rem'}}></img>
-                    <div className="card-body">
-                        <h5 className="card-title">{name.charAt(0).toUpperCase() + name.slice(1)}</h5>
-                        <h5>#{id}</h5>
-                        <h5>{capitalizedTypes.join(' ')}</h5>
-                    </div>
+                        <div className="backgroundEffect"></div>
+                        <img src={image} className="card-img-top" alt={name}
+                             style={{width: '11rem', height: '11rem'}}></img>
+                        <div className="card-body">
+                            <h5 className="card-title">{name.charAt(0).toUpperCase() + name.slice(1)}</h5>
+                            <h5>#{id}</h5>
+                            <h5 className={`type-box d-inline me-2 text-light ${capitalizedTypes[0].toLowerCase()}`}>{capitalizedTypes[0]}</h5>
+                            {capitalizedTypes[1] === undefined
+                                ? null : <h5 className={`type-box d-inline text-light ${capitalizedTypes[1].toLowerCase()}`}>{capitalizedTypes[1]}</h5>}
+                        </div>
                     </Link>
                 </div>
             </li>
